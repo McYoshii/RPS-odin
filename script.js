374c4796d26b377e2bebb1328e58c343c1393ab6
McYoshii <xzabrah@gmail.com>
2023-07-20 21:10:22 -0300

    general code cleanup in JS file.

--- script.js
+++ script.js
@@ -1,119 +1,88 @@
+//BELOW: IMPORTANT GAME VARIABLES + KEEPING COUNT OF THINGS
 let computer; //Empty variable that is to be overridden
 let player;   //Empty variable that is to be overridden
-let resultsTally = 0; //to be touched upon
+let resultsTally = 0; 
 let result;  
 let round = 0;
-let winAnnounce
+let winAnnounce;
 
-
-
+//BELOW: HOOKING UP HTML BUTTONS/TEXT WITH JS
 const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 const roundsText = document.querySelector("#roundsText");
 const winText = document.querySelector("#winText");
-
 const choiceBtns= document.querySelectorAll(".choiceBtn");
 
-
+//Button click logic
 choiceBtns.forEach(button => button.addEventListener('click', () => {
 
+    //text content that changes
     player = button.textContent; 
     getComputerChoice();
-
     playerText.textContent = `Player: ${player}`;
     computerText.textContent = `Computer: ${computer}`;
     resultText.textContent = checkWinner();
     roundsText.textContent = `Round ${round+=1}`;
     winText.textContent = counter();
-
     console.log(resultsTally);
     
-
+    //disables buttons after 5 rounds
     if (round === 5) {
-            console.log('Task performed!');
-
-            choiceBtns.forEach(button => button.disabled = true);
-            resultText.textContent = winAnnounce;
-            
-           
+            choiceBtns.forEach(button => button.disabled = true); 
+            resultText.textContent = winAnnounce;    
      }
-    
-
 }));
 
 
 
 function getComputerChoice (){
+
      const value = Math.floor(Math.random()*3)+1; //makes random computer selection
-
-
-    if (value===1){ // translates randomly generated number into a respective string
+    if (value === 1){ // translates randomly generated number into a respective string
         computer="rock"
         
-    } else if (value===2) {
+    } else if (value === 2) {
         computer="paper"
         
-    } else if (value===3) {
+    } else if (value === 3) {
         computer="scissors"
-    } else {
-        console.log("I think it brokin :( ") 
 
+    } else {
+        return;
     }
 }
 
-
 function checkWinner (){ // everything below this is responsible for the games overall functionality
-    let results = console.log('Results:\n\n PLAYER = ', player, "\n COMPUTER = ", computer,);
-    //let displayedPlayerScore = console.log(`TOTAL PLAYER SCORE: ${recordResults} POINTS`) //unused function for counter. this variable, as well as everywhere it is called is commented out and will be fixed.
-    
 
     if (player === computer){  
         return "Tie!"
-        //displayedPlayerScore;
         
     } else if (player == 'rock' && computer == "paper" ) {
         return "COMP WINS!"
-        //displayedPlayerScore;
-        
         
     } else if (player == 'rock' && computer == "scissors" ) {
         resultsTally+=1
         return "PLAYER WINS!!"
-        //displayedPlayerScore;
         
     } else if (player == 'paper' && computer == "scissors" ) {
         return "COMP WINS!"
-        //displayedPlayerScore;
 
     } else if (player == 'paper' && computer == "rock" ) {
         resultsTally+=1
         return "PLAYER WINS!"
-       
-        //displayedPlayerScore;
         
     } else if (player == 'scissors' && computer == "rock" ) {
         return "COMP WINS!"
-        //displayedPlayerScore;
-        
+
     } else if (player == 'scissors' && computer == "paper" ) {
         resultsTally+=1
         return "PLAYER WINS!"
-        
-        //displayedPlayerScore;
-        
+
     } else {
-        console.log(`ERROR: try inputting 'rock', 'paper' or 'scissors' instead.`)
+        return;
     }
-
-
 }
-
-
-
-
-
-
 
 function counter() {
     if (resultsTally>2.5){
@@ -126,5 +95,4 @@ function counter() {
     }
 }
 
-//repeatGame();
 
